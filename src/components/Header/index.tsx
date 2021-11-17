b2f3afe74767c0f51da53aa60b98a9118ea8ee15
Serhiy Korytskyy <korysergey55@gmail.com>
2021-11-17 12:26:09 +0200

    styles-done

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,20 +2,23 @@ import React from 'react'
 import { useHistory } from 'react-router'
 import { paths } from 'utils/paths'
 import styles from './styles.module.scss'
-import defaltLogo from 'sources/images/react.png'
 
-interface Iprops {
-  logo?: string
-  title?: string
-}
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faShippingFast } from '@fortawesome/free-solid-svg-icons'
 
-const Header: React.FC<Iprops> = ({ logo = defaltLogo, title = 'Header' }) => {
+
+const Header = ({ title = 'Shipping Fast' }) => {
   const history = useHistory()
   return (
     <header className={styles.header}>
       <div className={styles.container}>
         <a className={styles.link} onClick={() => history.push(paths.home)}>
-          <img className={styles.logo} src={logo} alt="logo" />
+          <FontAwesomeIcon
+            className={styles.iconAwesome}
+            icon={faShippingFast}
+            color="red"
+            size="2x"
+          />
         </a>
         <h2 className={styles.title}>{title}</h2>
       </div>
--- src/containers/Public/HomePage/index.tsx
+++ src/containers/Public/HomePage/index.tsx
@@ -1,13 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Header from 'components/Header'
 import styles from './styles.module.scss'
 
 const HomePage = () => {
+  const [myItems, setMyItems] = useState<string[]>([
+    'apple',
+    'avocado',
+    'apricot',
+    'pear',
+    'grapefruit',
+  ])
   return (
-    <div>
+    <div className={styles.homeContainer}>
       <Header />
+      <h2 className={styles.title}>Food delivery</h2>
       <div className={styles.container}>
-        <h2 className={styles.title}>HomePage</h2>
+        <ul className={styles.myList}>
+          <h2 className={styles.title}>My delivery-list</h2>
+          {myItems &&
+            myItems.map(item => <li className={styles.item}>{item}</li>)}
+        </ul>
+
+        <div className={styles.wripper}>
+          <ul className={styles.remuvebleList}>
+            <h2 className={styles.title}>Food-list</h2>
+            {myItems &&
+              myItems.map(item => <li className={styles.item}>{item}</li>)}
+          </ul>
+
+          <ul className={styles.constantList}>
+            <h2 className={styles.title}>Food-list-infinity</h2>
+            {myItems &&
+              myItems.map(item => <li className={styles.item}>{item}</li>)}
+          </ul>
+        </div>
       </div>
     </div>
   )
