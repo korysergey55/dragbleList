a57c904f09dcb11e3c49c37bccddd931610d1052
Serhiy Korytskyy <korysergey55@gmail.com>
2021-11-17 11:18:42 +0200

    Updates

---
+++ precache-manifest.23c91dcc85f993fa9ff962e701ef205b.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29ce688a874dcf3ca2d307e5d47d46aa",
+    "url": "/draggableList/index.html"
+  },
+  {
+    "revision": "5b0cf3b9525c29d1c2be",
+    "url": "/draggableList/static/css/2.5cc265e8.chunk.css"
+  },
+  {
+    "revision": "8625dfdcecb96ecd0e3c",
+    "url": "/draggableList/static/css/3.3f26578b.chunk.css"
+  },
+  {
+    "revision": "75b7dfbffe7070d2dcf4",
+    "url": "/draggableList/static/css/main.8cb1dcc5.chunk.css"
+  },
+  {
+    "revision": "5b0cf3b9525c29d1c2be",
+    "url": "/draggableList/static/js/2.ada31938.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/draggableList/static/js/2.ada31938.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8625dfdcecb96ecd0e3c",
+    "url": "/draggableList/static/js/3.ee5bae4d.chunk.js"
+  },
+  {
+    "revision": "75b7dfbffe7070d2dcf4",
+    "url": "/draggableList/static/js/main.2ff8f9ae.chunk.js"
+  },
+  {
+    "revision": "9284ab20b7c6b46c465e",
+    "url": "/draggableList/static/js/runtime-main.018fc920.js"
+  },
+  {
+    "revision": "dff0fa0a5db147915cdb4d9887893915",
+    "url": "/draggableList/static/media/404.dff0fa0a.jpg"
+  },
+  {
+    "revision": "81c2ac8f93c9fb8aa36bdab7282b86ac",
+    "url": "/draggableList/static/media/react.81c2ac8f.png"
+  }
+]);
